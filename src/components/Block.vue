e116fb9f5d4c9f171ee26fffcf86be0bb2eff336
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-08-29 19:33:03 +0300

    start the project and added block component

---
+++ src/components/Block.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="block">
+      Click Me...!
+      <p>{{delay}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['delay']
+}
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        color: white;
+        background: #0faf87;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
